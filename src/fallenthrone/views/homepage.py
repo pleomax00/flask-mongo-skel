7498308ae4e585001c5069630c798be40b302e59
Shamail Tayyab <shamail@noida.interrasystems.com>
2012-10-25 17:15:26 +0530

    Spawning working with flask

--- src/fallenthrone/views/homepage.py
+++ src/fallenthrone/views/homepage.py
@@ -8,24 +8,16 @@ from fallenthrone import app
 from flask import render_template
 from flask.views import MethodView
 from fallenthrone.models import User
-
-@app.route ("/")
-def index ():
-    name = "haha"
-    u = User (username = 'pleomax00', first_name = 'Shamail', last_name = 'Tayyab', email = 'pleomax00@gmail.com')
-    u.save ()
-    return render_template ('hello.jade', name=name)
+from flask import request
+import sys
 
 class Index (MethodView):
 
     def get (self):
         name = "yahoo"
-        #global request
-        #print request
-        print "ha"
         u = User (username = 'pleomax00', first_name = 'Shamail', last_name = 'Tayyab', email = 'pleomax00@gmail.com')
         u.save ()
         return render_template ('hello.jade', name=name)
 
-#index_view = Index.as_view ('index')
-#app.add_url_rule ('/', view_func = Index.as_view ('users'))
+index_view = Index.as_view ('index')
+app.add_url_rule ('/', view_func = Index.as_view ('users'))
--- src/spawning.sh
+++ src/spawning.sh
@@ -1,1 +1,2 @@
-spawning wsgi.application
+#! /usr/bin/env sh
+spawning -d --stderr="`pwd`/logs/stderr.log" --stdout="`pwd`/logs/stdout.log" wsgi.application --port 8765 -s 5 -t 0 --deadman=20 --reload=dev --max-age=30
